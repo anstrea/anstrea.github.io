36bf66af117ee96d482fcbcd88dfe627658abba9
anstrea <ucgading101@gmail.com>
2024-08-22 21:13:03 +0700

    script.js

--- Respon/Skibidi 1/script.js
+++ Respon/Skibidi 1/script.js
@@ -7,7 +7,7 @@ let nextClicked = false;
 
 nextBtn.addEventListener("click", () => {
     if (!nextClicked) {
-        question.innerHTML = "Thank you for loving me, with all my shortcomings. bercandaaa ";
+        question.innerHTML = "Thank you for loving me, with all my shortcomings. bercandaaa";
         gif.src= "https://raw.githubusercontent.com/anstrea/anstrea.github.io/main/Respon/Skibidi%201/thanks.gif";
         nextBtn.innerHTML = "reply";
         nextClicked = true;
