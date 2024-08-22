67fee6dc7681bc72d25651d0fe78c5bdbea283cd
anstrea <ucgading101@gmail.com>
2024-08-22 21:00:05 +0700

    script.js

--- Respon/Skibidi 1/script.js
+++ Respon/Skibidi 1/script.js
@@ -1,4 +1,5 @@
-const wrapper = document.querySelector(".wrapper"); const question = document.querySelector(".question");
+const wrapper = document.querySelector(".wrapper"); 
+const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const nextBtn = document.querySelector(".next-btn");
 
@@ -6,12 +7,10 @@ let nextClicked = false;
 
 nextBtn.addEventListener("click", () => {
     if (!nextClicked) {
-        question.innerHTML = "Thank you for loving
-        me, with all my shortcomings.";
+        question.innerHTML = "Thank you for loving me, with all my shortcomings.";
         gif.src= "https://raw.githubusercontent.com/anstrea/anstrea.github.io/main/Respon/Skibidi%201/thanks.gif";
-        nextBtn.innerHTML = "reply":
+        nextBtn.innerHTML = "reply";
         nextClicked = true;
-        
     } else {
         window.location.href= "https://wa.me/6282245578978?text=Terserah%20Mau%20Reply%20Apaaa";
 }
